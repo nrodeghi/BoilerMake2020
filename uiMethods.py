6883ac781a43c3ba3a761fa6f46a416a2dd4eec2
Natalie Rodeghier <42589066+nrodeghi@users.noreply.github.com>
2020-01-24 23:40:41 -0500

    my code opens my ui!

--- uiMethods.py
+++ uiMethods.py
@@ -1,11 +1,18 @@
 import sys
 
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 from uiCode import Ui_MainWindow
 
-class MyForm(QtGui.QMainWindow):
+class MyForm(QtWidgets.QMainWindow):
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        super(MyForm, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyForm()
+    myapp.show()
+    sys.exit(app.exec_())
